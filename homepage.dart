333799600a9256a46f2601693fc039299f748ed2
arthhar <108536052+arthhar@users.noreply.github.com>
2023-02-12 20:42:48 +0530

    Create homepage.dart

---
+++ homepage.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:helloworld/account.dart';
+import 'package:helloworld/home.dart';
+import 'package:helloworld/reels.dart';
+import 'package:helloworld/search.dart';
+import 'package:helloworld/shop.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 2;
+  void _navigateBottomNavBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _children = [
+    UserHome(),
+    UserSearch(),
+    UserReels(),
+    UserShop(),
+    UserAccount(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('body here')),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _navigateBottomNavBar,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_call), label: 'reels'),
+          BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'account'),
+        ],
+      ),
+    );
+  }
+}
